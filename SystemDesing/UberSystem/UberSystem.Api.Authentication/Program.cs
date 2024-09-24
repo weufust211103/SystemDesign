3a9733c20553fa9023e1910b15486312d68aff57
SE173544_NguyenDoTrungQuan1 <quanndtse173544@fpt.edu.vn>
2024-09-24 14:16:58 +0700

    sdfasd
    sdasd

--- SystemDesing/UberSystem/UberSystem.Api.Authentication/Program.cs
+++ SystemDesing/UberSystem/UberSystem.Api.Authentication/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 using UberSystem.Api.Authentication.Extensions;
+using UberSystem.Domain.Entities;
+using UberSystem.Domain.Interfaces.Services;
+using UberSystem.Infrastructure;
+using UberSystem.Service;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<UberSystemDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register Identity
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<UberSystemDbContext>()
+    .AddDefaultTokenProviders();
+
+// Register IUserService
+builder.Services.AddScoped<IUserService, UserService>();
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -28,9 +45,34 @@ builder.Services.AddSwaggerGen(opt =>
     // Set the comments path for the Swagger JSON and UI.
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    //Add bearer
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
 });
 var connectionString = builder.Configuration.GetConnectionString("Default");
 var configuration = builder.Configuration;
+
 //DI services
 builder.Services.AddDatabase(configuration).AddServices();
 //builder.Services.AddAutoMapperFramework();
--- SystemDesing/UberSystem/UberSystem.Api.Customer/Program.cs
+++ SystemDesing/UberSystem/UberSystem.Api.Customer/Program.cs
@@ -1,11 +1,24 @@
-﻿using UberSystem.Api.Customer.Extensions;
+﻿using Microsoft.EntityFrameworkCore;
+using UberSystem.Api.Customer.Extensions;
+using UberSystem.Domain.Interfaces.Services;
+using UberSystem.Domain.Interfaces;
+using UberSystem.Infrastructure;
+using UberSystem.Service;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<DbFactory>(); // Register DbFactory
+builder.Services.AddDbContext<UberSystemDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-// Add services to the container.
-builder.Services.Register(builder.Configuration);
-//builder.Services.AddAutoMapperFramework();
+// Register AutoMapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+builder.Services.AddScoped<Func<UberSystemDbContext>>(provider => () => provider.GetService<UberSystemDbContext>());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
